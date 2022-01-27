ed9d28393557bb5b80461d6eb5044294d8bee44d
kallen312 <73615486+kallen312@users.noreply.github.com>
2022-01-26 20:32:40 -0500

    finish day 1 goal

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,22 @@ import "../css/App.css";
 import data from "../sample_data.json";
 
 function App() {
-  return <div className="app">Trivia!</div>;
+  let varible = 0;
+  return (
+    <div className="app">
+      <h1>Trivia!</h1>
+      <Question question={data[varible].question.text} />
+      <NextQuestion />
+    </div>
+  );
+}
+
+function NextQuestion(props) {
+  return <button>Next Question</button>;
+}
+
+function Question(props) {
+  return <p>{props.question}</p>;
 }
 
 export default App;
